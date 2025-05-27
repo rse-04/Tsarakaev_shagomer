1beb6055874b8f50828947103994445b20a8633d
rse0003 <sssrse.s0001@gmail.com>
2025-05-27 16:12:11 +0300

    Update kirill_02.ino

--- kirill_02/kirill_02.ino
+++ kirill_02/kirill_02.ino
@@ -24,8 +24,10 @@ void data(){
 }
 
 //способности: сбор данных о шагах 
-//ывод данные на экран(vivmat)
-void ecran(a){}
+//ывод данные на экран(TM1637)
+void ecran(){
+   
+}
 
 //проверитть кноп 1()
 
@@ -43,7 +45,7 @@ void knop1(){
   }
   }
 
-}
+
 void knop2(){
  int sensorVal = digitalRead(KNOP2);
   Serial.println(sensorVal);  // Выводим значение в мониторе порта
@@ -51,8 +53,6 @@ void knop2(){
   if (sensorVal == 0) {
     k2=1;
   }
-  }
-
 }
 
 void reset(){
@@ -64,65 +64,60 @@ void reset(){
 #define PIN_INPUT 2
 #define PIN_LED 13
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(PIN_INPUT, INPUT);
-  pinMode(PIN_LED, OUTPUT);
-  Serial.begin(9600);
-  pinMode(PIN_INPUT, INPUT);
-  pinMode(PIN_LED, OUTPUT);
-}
-
-void loop() {
-  int sensorVal = digitalRead(PIN_INPUT);
-  Serial.println(sensorVal);  // Выводим значение в мониторе порта
-  // Логика работы кнопки такова: если кнопка не нажата, то на вход поступает высокий уровень сигнала, если нажали — то на входе будет логический ноль
-  if (sensorVal == HIGH) {
-    digitalWrite(PIN_LED, LOW);  // Кнопка не нажата — выключаем светодиод
-  } else {
-    digitalWrite(PIN_LED, HIGH);// А теперь кнопка нажата — светодиод включается
-  }
-
-
-
 
 void setup() {
-  int s=1
+  int s=1;
    Serial.begin(9600);
     accel.initialize();
-    Serial.println(accel.testConnection()// "MPU6050 connection successful" : "MPU6050 connection failed");
+    Serial.println(accel.testConnection());     // "MPU6050 connection successful" : "MPU6050 connection failed");
+    Serial.begin(9600);
+  pinMode(PIN_INPUT, INPUT);
+  pinMode(PIN_LED, OUTPUT);
+  Serial.begin(9600);
+  pinMode(PIN_INPUT, INPUT);
+  pinMode(PIN_LED, OUTPUT);
 }
 
 void loop() {
-  knop1();
-  knop2();
 
-  //тригеры
-  //реакции
-//   0. ждать
-   if (s==0) {
-       datch(); 
-      if (k1==1 or k2==1){
-        s=1;
-      }
-   }
--
-  // 2. вывести данные
-      if (s==1) {
-         if (k1==1){
-          ecran(zaryad)
-         }
-          if (k2==1){
-          ecran(shagi)
-         }
-         reset()
-         delay(2000)
-         s=1;
-     }
+  // int sensorVal = digitalRead(PIN_INPUT);
+  // Serial.println(sensorVal);  // Выводим значение в мониторе порта
+  // // Логика работы кнопки такова: если кнопка не нажата, то на вход поступает высокий уровень сигнала, если нажали — то на входе будет логический ноль
+  // if (sensorVal == HIGH) {
+  //   digitalWrite(PIN_LED, LOW);  // Кнопка не нажата — выключаем светодиод
+  // } else {
+  //   digitalWrite(PIN_LED, HIGH);// А теперь кнопка нажата — светодиод включается
+//   // }
 
-     // 3. Разряжен;
-     if (s==2) {
+//   knop1();
+//   knop2();
+
+//   //тригеры
+//   //реакции
+// //   0. ждать
+//    if (s==0) {
+//        datch(); 
+//       if (k1==1 or k2==1){
+//         s=1;
+//       }
+//    }
+// -
+//   // 2. вывести данные
+//       if (s==1) {
+//          if (k1==1){
+//           ecran(zaryad)
+//          }
+//           if (k2==1){
+//           ecran(shagi)
+//          }
+//          reset()
+//          delay(2000)
+//          s=1;
+//      }
+
+//      // 3. Разряжен;
+//      if (s==2) {
 
       
-    }
+//     }
 }
